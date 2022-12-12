c05b5d8bacdf1d1e4666ebf62ba26728eb54bd80
andreykhlivniuk <khlivnia@tcd.ie>
2022-12-12 12:49:09 +0200

    requests

--- jenkins_test.py
+++ jenkins_test.py
@@ -1,1 +1,28 @@
-print("hello world")
+import requests
+
+headers={
+
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+}
+
+data ={
+  "id": 0,
+  "category": {
+    "id": 12,
+    "name": "dog"
+  },
+  "name": "puppy",
+  "photoUrls": [
+    "none"
+  ],
+  "tags": [
+    {
+      "id": 0,
+      "name": "newdog"
+    }
+  ],
+  "status": "available"
+}
+
+response = requests.post("https://petstore.swagger.io/v2/pet", json = data, headers = headers)
+pet_id = response.json()["id"]
